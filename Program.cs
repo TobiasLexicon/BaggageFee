d0f38927e398b94cb03d944fded1c5521fb25d5e
Exilic <tobias.engberg@exilic.se>
2022-03-24 14:30:50 +0100

    Changed method naming

--- Program.cs
+++ Program.cs
@@ -15,25 +15,23 @@ namespace BaggageFee
 
         static void Main(string[] args)
         {
-            int weight = checkBaggage();
-            int excessWeight = getExcessWeight(weight);
-            int feeToPay = computeAdditionalFee(excessWeight);
+            int weight = CheckBaggage();
+            int excessWeight = GetExcessWeight(weight);
+            int feeToPay = ComputeAdditionalFee(excessWeight);
 
-            int additionalFee2 = computeAdditionalFee(getExcessWeight(checkBaggage()));
+            int additionalFee2 = ComputeAdditionalFee(GetExcessWeight(CheckBaggage()));
 
-            int additionalFee = checkBaggage()
-                .getExcessWeight()
-                .computeAdditionalFee();
-                
+            int additionalFee = CheckBaggage()
+                .GetExcessWeight()
+                .ComputeAdditionalFee();
 
+            WriteLine("More text");
             WriteLine($"You have to pay {feeToPay}$ in baggage fee");
             ReadKey();
 
-
- 
         }
 
-        public static int checkBaggage()
+        public static int CheckBaggage()
         {
             int registeredWeight = 0;
             bool validInput = false;
@@ -45,7 +43,7 @@ namespace BaggageFee
             return registeredWeight;
         }
 
-        public static int getExcessWeight(this int registeredWeight)
+        public static int GetExcessWeight(this int registeredWeight)
         {
             int excessWeight = 0;
             int allowedWeight = 12;
@@ -56,7 +54,7 @@ namespace BaggageFee
             return excessWeight;
         }
 
-        public static int computeAdditionalFee(this int excessWeight)
+        public static int ComputeAdditionalFee(this int excessWeight)
         {
             int additionalFee = 36;
             int feePerKilo = 5;
